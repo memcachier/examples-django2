099bc15c7ded17819481f18da411f9f9c01486f0
David Terei <davidterei@gmail.com>
2013-06-09 02:47:32 -0400

    fix some bugs with static files

--- django_line/urls.py
+++ django_line/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^add$', 'line.views.add'),
     url(r'^remove$', 'line.views.remove'),
     url(r'^$', 'line.views.index'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
